289467f9f7fea2ee420f4d5bbcf6d54e1f5e16bf
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-18 01:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,28 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+
+var itemNP = generateCartItem(item)
+
+getCart().push(item)
+
+return `${item.itemName} has been added to your cart.`
+}
+
+function generateCartItem(itemName) {
+  return itemName = itemName,
+  itemPrice = getRandomInt(1, 100)
+  }
+
+function getRandomInt(min, max) {
+  return math.floor(math.random() * (max-min + 1) + min)
 }
 
 function viewCart() {
   // write your code here
-}
+  return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
+
+
 
 function total() {
   // write your code here
